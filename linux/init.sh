d3410918ede9c481407b5bfa64438e91500c2ac0
talentestors <talentestors@gmail.com>
2024-11-16 23:10:26 +0800

    feat: ✨ add `autosuggestions` and `z` plugins for zsh config
    add `autosuggestions` plugins config
    add `z` plugins config

---
+++ linux/init.sh
@@ -0,0 +1,1 @@
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
--- windows/msys2/user/install.ps1
+++ windows/msys2/user/install.ps1
@@ -1,2 +1,3 @@
 git clone --depth 1 git@github.com:ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth 1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
