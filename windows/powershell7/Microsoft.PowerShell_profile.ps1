e9883b148735c24973e432a086e357f2b38274d6
talentestors <talentestors@gmail.com>
2024-12-24 17:08:16 +0800

    Update powershell7

--- windows/powershell7/Microsoft.PowerShell_profile.ps1
+++ windows/powershell7/Microsoft.PowerShell_profile.ps1
@@ -99,3 +99,10 @@ Function which_gcm {
 }
 
 Set-Alias -Name which -Value which_gcm
+
+
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
