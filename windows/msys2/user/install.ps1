7c10251b167ac5548e369488dc509f949716e390
talentestors <talentestors@gmail.com>
2024-12-27 23:17:30 +0800

    ✨ Improve PowerShell script to ensure sequential execution of commands
    - Added check to only execute the second git clone if the first one succeeds
    - Included English message output for better clarity on failure

--- windows/msys2/user/install.ps1
+++ windows/msys2/user/install.ps1
@@ -1,2 +1,8 @@
+# Clone Oh My Zsh
 git clone --depth 1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+if ($?) {
+	git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $env:ZSH_CUSTOM/plugins/zsh-autosuggestions
+} else {
+	 Write-Host "The first command failed; the second command will not be executed."
+}
